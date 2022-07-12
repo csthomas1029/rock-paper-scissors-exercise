1072e083e41dd94858e6d9c144b79ca4d6632a7a
csthomas1029 <csthomas1029@gmail.com>
2022-07-12 19:46:21 -0400

    Updated game.py with requirement sections

--- game.py
+++ game.py
@@ -1,3 +1,31 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+# USER INPUTS
+
+
+# VALIDATE USER INPUTS
+
+
+# SIMULATE COMPUTER SELECTION
+
+
+# DETERMINE THE WINNER
+
+
+# DISPLAY THE RESULTS
+
+
+# -------------------
+# Welcome 'Player One' to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
