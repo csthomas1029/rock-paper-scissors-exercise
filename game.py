3998a4e756aab0873f0bc1fac27b4e57ccdebf45
csthomas1029 <csthomas1029@gmail.com>
2022-07-13 15:09:31 -0400

    Added a farewell message in game.py

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 # this is the "game.py" file...
+# inspiration for code language from https://nyu-tech-2335.slack.com/archives/C5WPFSB52
 import random
 
 print("Rock, Paper, Scissors, Shoot!")
 
 
-
 # USER INPUTS
 player_choice = input("Welcome! Please make a selection ('rock', 'paper', 'scissors'): ")
 player_choice = player_choice.lower()
+# this was every time we use the player_choice it will be lowercase
 
 print("You chose:", player_choice)
 
@@ -18,14 +19,15 @@ valid_choices = ["rock", "paper", "scissors"]
 if player_choice not in valid_choices:
     print("Invalid. Please ensure you make a valid selection.")
     exit()
-    
+
+
 # SIMULATE COMPUTER SELECTION
 
 computer_choice = random.choice(valid_choices)
 print("Computer chose:", computer_choice)
 
+
 # DETERMINE THE WINNER
-# inspiration for code language from https://nyu-tech-2335.slack.com/archives/C5WPFSB52
 if player_choice == computer_choice:
     print("You both chose ", player_choice,". It's a tie!")
 elif player_choice == "rock" and computer_choice == "paper":
@@ -41,19 +43,8 @@ elif player_choice == "scissors" and computer_choice == "rock":
 elif player_choice == "scissors" and computer_choice == "paper":
     print("Congrats! You beat the computer.")
 else:
-    print("Invalid. Please note that the selections are case sensitive.")
+    exit()
 
 
 # DISPLAY THE RESULTS
-
-
-# -------------------
-# Welcome 'Player One' to my Rock-Paper-Scissors game...
-# -------------------
-# Please choose either 'rock', 'paper', or 'scissors': rock
-# You chose: 'rock'
-# The computer chose: 'paper'
-# -------------------
-# Oh, the computer won. It's ok.
-# -------------------
-# Thanks for playing. Please play again!
+print("Thanks for playing and please come play again!")
